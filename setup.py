fc4f47de3cb540ea1731c47b09303a413bec2acc
Miguel Ferrer <ingferrermiguel@gmail.com>
2019-08-20 16:01:27 -0500

    Bump ver. 0.1.2

--- setup.py
+++ setup.py
@@ -7,9 +7,10 @@ def read(fname):
 
 
 setup(name='surveymonkey-python',
-      version='0.1.1',
+      version='0.1.2',
       description='Python wrapper for SurveyMonkey API',
       long_description=read('README.md'),
+      long_description_content_type="text/markdown",
       url='https://github.com/GearPlug/surveymonkey-python',
       author='Nerio Rincon',
       author_email='nrincon.mr@gmail.com',
