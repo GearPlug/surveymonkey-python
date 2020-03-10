fa111f932dd7c51e93cab415c37aaa70886c89d7
Miguel Ferrer <ingferrermiguel@gmail.com>
2020-03-10 14:05:44 -0500

    Bump ver. 0.1.3

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ def read(fname):
 
 
 setup(name='surveymonkey-python',
-      version='0.1.2',
+      version='0.1.3',
       description='Python wrapper for SurveyMonkey API',
       long_description=read('README.md'),
       long_description_content_type="text/markdown",
       url='https://github.com/GearPlug/surveymonkey-python',
-      author='Nerio Rincon',
-      author_email='nrincon.mr@gmail.com',
-      license='GPL',
+      author='Miguel Ferrer',
+      author_email='ingferrermiguel@gmail.com',
+      license='MIT',
       packages=['surveymonkey'],
       install_requires=[
           'requests',
